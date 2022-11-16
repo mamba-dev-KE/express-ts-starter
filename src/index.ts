fe1dfcdb6ee6019db79591466f79214b06b42d18
Joseph Maramba <josephmaramba50@gmail.com>
2022-11-16 13:15:32 +0300

    add shopify eslint rules and reconfig

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5500;
 
 app.get('/', (_, res: Response) => {
