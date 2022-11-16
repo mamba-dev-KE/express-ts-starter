d10abb83138a0915ad359e3806474f9bd95a83cd
Joseph Maramba <josephmaramba50@gmail.com>
2022-11-16 12:22:13 +0300

    config server and install express, dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world mamba');
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 5500;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'succes' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
